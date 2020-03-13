3df52bae794554ff1c05c9de630c4fa8ded0dce6
gyf210 <316036643@qq.com>
2020-03-14 01:54:34 +0800

    change hello

--- echo/echo.go
+++ echo/echo.go
@@ -2,6 +2,13 @@ package echo
 
 import "fmt"
 
-func Hello(name string) {
-	fmt.Printf("Hello, %s\n", name)
+func Hello(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hello, %s\n", name), nil
+	case "zh":
+		return fmt.Sprintf("你好, %s\n", name), nil
+	default:
+		return "", fmt.Errorf("unknow lang")
+	}
 }
